b825935aecdabeaf7f35fe4469704714a7cc7415
mepox <21198248+mepox@users.noreply.github.com>
2022-08-26 16:24:09 +0100

    Initial commit

---
+++ src/main/java/com/laszlojanku/spring/workouttracker/SimpleWorkoutTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.laszlojanku.spring.workouttracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleWorkoutTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleWorkoutTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laszlojanku/spring/workouttracker/SimpleWorkoutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laszlojanku.spring.workouttracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleWorkoutTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
